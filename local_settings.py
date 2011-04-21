3bd0c7e049564d74f39f3c6da5a28dce399cd9ca
Eric van Zanten <eric@bahai.us>
2011-04-21 10:18:25 +0000

    Really the local_settings.py file

--- local_settings.py
+++ local_settings.py
@@ -7,11 +7,11 @@ DATABASES = {
         # "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
         "ENGINE": "postgresql_psycopg2",
         # DB name or path to database file if using sqlite3.
-        "NAME": "natalie",
+        "NAME": "",
         # Not used with sqlite3.
-        "USER": "natalie",
+        "USER": "",
         # Not used with sqlite3.
-        "PASSWORD": "thKQyaP1Ny1DSyCLRA",
+        "PASSWORD": "",
         # Set to empty string for localhost. Not used with sqlite3.
         "HOST": "",
          # Set to empty string for default. Not used with sqlite3.
